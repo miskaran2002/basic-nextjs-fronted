3da53432e0d682cd7287bedfd30edbebac820536
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-24 16:57:56 +0600

    linking &navigation

---
+++ app/blogs/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogsLoader = () => {
+  return (
+    <div>Loading....</div>
+  )
+}
+
+export default BlogsLoader
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
 
 
 export default function Home() {
   return (
-    <div>hello Next js</div>
+    <div>hello Next js
+      Blog page <Link href="/blogs">Go to blogs</Link>
+     
+    </div>
+    
   );
 }
