52de4c8277f8e96edfe2da31fabf1d302ff4b74a
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-24 17:31:09 +0600

    app router vs post router

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
-const BlogsPage = () => {
+const BlogsPage =async () => {
+
+    const post = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    const data = await post.json();
+
+    console.log (data);
+
+
+
   return (
     <div> Blogs Page</div>
   )
