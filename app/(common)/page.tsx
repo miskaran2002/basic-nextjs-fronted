63cef30371db2ca9c6f34180c546658f4259515d
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-25 21:31:48 +0600

    project setup

--- app/(common)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CommonPage = () => {
-  return (
-    <div>CommonPage</div>
-  )
-}
-
-export default CommonPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import Link from "next/link"
-import LikeButton from "./ui/LikeButton";
+
+
 
 
 
 export default function Home() {
   return (
-    <div>hello Next js
-      Blog page <Link href="/blogs">Go to blogs</Link>
-
-      
-      <LikeButton />
-    </div>
+    <div>Hello Nextjs!!!</div>
     
   );
 }
