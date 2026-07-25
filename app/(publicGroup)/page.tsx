c3825e0a79c6c040d3bfc955559b92f95ee36d91
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-25 22:53:25 +0600

    installing sadcsn setup

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,11 +1,21 @@
-
-
-
-
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <div>Hello Nextjs!!!</div>
+    <div>
+      Hello Nextjs!!!
+      <Button
+      size={"lg"}
+     variant={"destructive"}
+      >
+      Click me
+      </Button>
+
+    </div>
+
+    
+    
+    // <div className="flex flex-col items-center justify-center min-h-screen py-2">
     
   );
 }
