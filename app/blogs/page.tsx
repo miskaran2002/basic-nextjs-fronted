ad17c63cd4a8f69000b9d2c6941bb5712b4632c0
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-07-25 13:52:17 +0600

    cache component

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import MyServerComponent from '../ui/MyServerComponent';
+import { getBlogs } from '../service/getBlogs';
+import { cacheLife, cacheTag } from 'next/cache';
 
 const BlogsPage =async () => {
+  // "use cache";
+  //   cacheLife("hours")
 
-    const post = await fetch('https://jsonplaceholder.typicode.com/posts');
+  
 
-    const data = await post.json();
-
-    console.log (data);
-
-
-
+   
+const blogs = await getBlogs();
+console.log(blogs);
   return (
     <div> 
       Blogs Page
+      {
+        blogs.map((blog:any) => (
+          <div key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.body}</p>
+          </div>
+        ))
+      }
+
       <MyServerComponent/>
 
     </div>
