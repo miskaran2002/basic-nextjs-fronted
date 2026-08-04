df06128f4e8c7df8620ac589f2cfde87ea3b2d9c
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-08-04 15:19:04 +0600

    update proxy

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,47 @@
+// import { cookies } from "next/dist/server/request/cookies";
 import {NextResponse} from "next/server"
 import type { NextRequest } from "next/server"
+import jwt,{JwtPayload} from "jsonwebtoken"
+
+
+
+const AUTH_ROUTES =["/login","/register"]
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
-    console.log(request.nextUrl,"request");
-    console.log("Proxy Middleware Triggered");
-    console.log(pathname,"pathname");
-    return NextResponse.redirect(new URL('/', request.url));
+
+    // const cookieStore = await cookies();
+
+    const accessToken = request.cookies.get("accessToken")?.value;
+
+   const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+   let userRole = null;
+   if (decodedToken ) {
+       userRole = decodedToken.role;
+   }
+   if(accessToken && AUTH_ROUTES.includes(pathname)){
+
+    if (userRole === "USER") {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    } else if (userRole === "ADMIN") {
+        return NextResponse.redirect(new    URL('/admin-dashboard', request.url));
+    } else if (userRole === "AUTHOR") {
+        return NextResponse.redirect(new URL('/author-dashboard', request.url));
+    } else {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+   }
+    
+    // return NextResponse.redirect(new URL('/', request.url));
+
+    return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        '/dashboard/:path*',
-        '/admin-dashboard/:path*',
-       
-    ],
+  matcher: [
+    '/((?!api|_next/static|favicon.ico|next/image|.*\\.png$).*)'
+  ],
+}
     
-} 
